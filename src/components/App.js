1585160c3b91771bc11756411834762bbc008d2d
EladRon <elad.ron.g@gmail.com>
2019-12-18 17:51:52 +0200

    putting it all together

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../api/youtube';
 
 class App extends React.Component {
+    onTermSubmit = (term) => {
+        youtube.get('/search', {
+            params: {
+                q: term
+            }
+        });
+    };
+
+
     render() {
         return (
-            <div><SearchBar /></div>
+            <div className="ui container">
+                <SearchBar onFormSubmit={this.onTermSubmit} />
+            </div>
         );
     }
 }
